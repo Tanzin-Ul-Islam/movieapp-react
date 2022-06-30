4c7bcc23e0b31f6e297085f138e6d81bad27557d
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2022-06-30 17:18:06 +0600

    Random quote inti

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./app.css"
+export default function App() {
+    let [quote, setQuote] = useState("");
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+    const fetchQuote = () => {
+        axios.get('https://api.adviceslip.com/advice').then((response) => {
+            let data = response.data.slip.advice
+            setQuote(quote = data);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+    return (
+        <div className='app'>
+            <div className='card'>
+                <h3 className='heading'>{quote}</h3>
+                <button className='button' onClick={()=>{fetchQuote()}}>Give me advice</button>
+            </div>
+        </div>
+    )
+}
